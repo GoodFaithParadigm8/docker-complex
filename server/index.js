9f9c8cddf2f963e8477db8c8d2f87273462941eb
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-01-10 21:29:54 -0600

    updated readme , fixed typo in index

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ app.post('/values', async (req, res) => {
     redisPublisher.publish('insert', index);
     pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    redis.send({working: true});
+    res.send({working: true});
 });
 
 app.listen(5000, err => {
